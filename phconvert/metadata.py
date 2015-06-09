9687085bb75c8445bbbf61e80d42a6b844156f26
Antonino Ingargiola <tritemio@gmail.com>
2015-06-09 10:34:17 -0700

    Bump format version to 0.4

--- phconvert/metadata.py
+++ phconvert/metadata.py
@@ -13,7 +13,7 @@ import pkg_resources
 from collections import OrderedDict
 import json
 
-LATEST_FORMAT_VERSION = '0.4dev'
+LATEST_FORMAT_VERSION = '0.4'
 
 _specs_file_fields = 'specs/photon-hdf5_fields.json'
 
