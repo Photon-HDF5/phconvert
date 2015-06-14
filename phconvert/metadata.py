7d671eec6720f4afee7b9c8d363d452bfa474d2d
Antonino Ingargiola <tritemio@gmail.com>
2015-06-14 13:02:25 -0700

    Always use binary strings (both in py2 and py3) to maximize compatibility.

--- phconvert/metadata.py
+++ phconvert/metadata.py
@@ -13,7 +13,7 @@ import pkg_resources
 from collections import OrderedDict
 import json
 
-LATEST_FORMAT_VERSION = '0.4'
+LATEST_FORMAT_VERSION = b'0.4'
 
 _specs_file_fields = 'specs/photon-hdf5_fields.json'
 
@@ -21,15 +21,15 @@ _specs_file_fields = 'specs/photon-hdf5_fields.json'
 def _get_fields_descr():
     s = pkg_resources.resource_string('phconvert',
                                       _specs_file_fields).decode('utf8')
-    descr = {k: v if isinstance(v, str) else v.encode()
+    descr = {k: v.encode()
              for k, v in json.loads(s).items()}
     return descr
 
 # Metadata for the HDF5 root node
 root_attributes = OrderedDict([
-    ('format_name', 'Photon-HDF5'),
+    ('format_name', b'Photon-HDF5'),
     ('format_version', LATEST_FORMAT_VERSION),
-    ('format_url', 'http://photon-hdf5.readthedocs.org/'),
+    ('format_url', b'http://photon-hdf5.readthedocs.org/'),
 ])
 
 
