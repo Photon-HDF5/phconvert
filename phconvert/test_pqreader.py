a3f884f3b5a21568fe0fab69e7acfe68659a0120
Antonino Ingargiola <tritemio@gmail.com>
2018-11-28 12:13:51 +0100

    Tests: refactor PTU tests and add new data file

--- phconvert/test_pqreader.py
+++ phconvert/test_pqreader.py
@@ -1,6 +1,6 @@
 from __future__ import division
-from builtins import range, zip
 import os
+import numpy as np
 
 import pytest
 import phconvert as phc
@@ -9,56 +9,90 @@ DATADIR = 'notebooks/data/'
 
 
 def dataset1():
-    fn = '161128_DM1_50pM_pH74.ptu'
+    fn = '161128_DM1_50pM_pH74.ptu'  # 'rtHydraHarp2T3'
     fname = DATADIR + fn
     return fname
+
 
 def dataset2():
-    fn = '20161027_DM1_1nM_pH7_20MHz1.ptu'
+    fn = '20161027_DM1_1nM_pH7_20MHz1.ptu'  # 'rtHydraHarp2T3'
     fname = DATADIR + fn
     return fname
+
 
 def dataset3():
-    fn = 'Cy3+Cy5_diff_PIE-FRET.ptu'
+    fn = 'Cy3+Cy5_diff_PIE-FRET.ptu'  # 'rtPicoHarpT3'
     fname = DATADIR + fn
     return fname
 
-@pytest.fixture(scope="module", params=[dataset1, dataset2, dataset3])
+
+def dataset4():
+    fn = 'TestFile_2.ptu'  # 'rtHydraHarpT3'
+    fname = DATADIR + fn
+    return fname
+
+
+@pytest.fixture(scope="module",
+                params=[dataset1, dataset2, dataset3, dataset4])
 def filename(request):
     fname = request.param()
     return fname
+
+
+def test_read_ptu_recordtype(filename):
+    """Test for PTU header decoding."""
+    assert os.path.isfile(filename), 'File not found: %s' % filename
+    t3records, timestamps_unit, nanotimes_unit, record_type, tags = \
+        phc.pqreader.ptu_reader(filename)
+    assert list(tags.keys())[-1] == "Header_End"
+    assert timestamps_unit == 1 / tags['TTResult_SyncRate']['value']
+    assert nanotimes_unit == tags['MeasDesc_Resolution']['value']
+    ptu_rec_code = {0x00010304: 'rtHydraHarpT3',
+                    0x01010304: 'rtHydraHarp2T3',
+                    0x00010303: 'rtPicoHarpT3',
+                    }
+    rec_type2 = ptu_rec_code[tags['TTResultFormat_TTTRRecType']['value']]
+    assert rec_type2 == record_type
+
+
+def test_read_ptu_overflow_func(filename):
+    """Test for PTU overflow correction."""
+    assert os.path.isfile(filename), 'File not found: %s' % filename
+    t3records, timestamps_unit, nanotimes_unit, record_type, tags = \
+        phc.pqreader.ptu_reader(filename)
+    if record_type == 'rtHydraHarp2T3':
+        det, ts, nanot = phc.pqreader.process_t3records(
+            t3records[:1000000], time_bit=10, dtime_bit=15, ch_bit=6,
+            special_bit=True, ovcfunc=phc.pqreader._correct_overflow_nsync)
+        det2, ts2, nanot2 = phc.pqreader.process_t3records(
+            t3records[:1000000], time_bit=10, dtime_bit=15, ch_bit=6,
+            special_bit=True, ovcfunc=phc.pqreader._correct_overflow_nsync_naive)
+        assert (det == det2).all()
+        assert (ts == ts2).all()
+        assert (nanot == nanot2).all()
 
 
 def test_load_ptu(filename):
     """Smoke test."""
     assert os.path.isfile(filename), 'File not found: %s' % filename
     timestamps, detectors, nanotimes, meta = phc.pqreader.load_ptu(filename)
+    # Test metadata
     tags = meta['tags']
     acq_duration = tags['MeasDesc_AcquisitionTime']['value'] * 1e-3
-    acq_duration_exp = (timestamps[-1] - timestamps[0]) * meta['timestamps_unit']
+    acq_duration_exp = ((timestamps[-1] - timestamps[0])
+                        * meta['timestamps_unit'])
     assert abs(acq_duration - acq_duration_exp) < 0.1
-
-
-def test_read_ptu(filename):
-    """Test for PTU header decoding."""
-    t3records, timestamps_unit, nanotimes_unit, record_type, tags = \
-        phc.pqreader.ptu_reader(filename)
-    assert list(tags.keys())[-1] == "Header_End"
-    assert timestamps_unit == 1 / tags['TTResult_SyncRate']['value']
-    assert nanotimes_unit == tags['MeasDesc_Resolution']['value']
-    ptu_rec_code = {0x01010304: 'rtHydraHarp2T3', 0x00010303: 'rtPicoHarpT3'}
-    rec_type2 = ptu_rec_code[tags['TTResultFormat_TTTRRecType']['value']]
-    assert rec_type2 == record_type
-    if record_type == 'rtHydraHarp2T3':
-        det, ts, nanot = phc.pqreader.process_t3records(t3records[:1000000],
-                    time_bit=10, dtime_bit=15, ch_bit=6, special_bit=True,
-                    ovcfunc=phc.pqreader._correct_overflow_nsync)
-        det2, ts2, nanot2 = phc.pqreader.process_t3records(t3records[:1000000],
-                    time_bit=10, dtime_bit=15, ch_bit=6, special_bit=True,
-                    ovcfunc=phc.pqreader._correct_overflow_nsync_naive)
-        assert (det == det2).all()
-        assert (ts == ts2).all()
-        assert (nanot == nanot2).all()
+    assert 'laser_repetition_rate' in meta
+    assert 1e6 < meta['laser_repetition_rate'] > 10e6
+    for field in ('software', 'software_version', 'creation_time',
+                  'hardware_name'):
+        assert field in meta
+        assert isinstance(field, str)
+    # Test photon data
+    assert ((nanotimes >= 0) * (nanotimes < 4096)).all()
+    valid = detectors != 127
+    timestamps = timestamps[valid]
+    assert (np.diff(timestamps) >= 0).all()
 
 
 def test_load_ht3():
