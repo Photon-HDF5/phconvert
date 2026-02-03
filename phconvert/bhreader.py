Fixed non-numba overflow functions, improved tests

phconvert/bhreader.py
@@ -521,7 +521,7 @@ def _read_32_nonQC_records(records:np.ndarray[np.uint32],
     diferent specification for markers and overflows.
     Specify field locations with bitmask and shift arguments
     """
-    timestamps = _mask_shift(records, timestamps_bitmask, timestamps_shift, '<i8')
+    timestamps = _mask_shift(records, timestamps_bitmask, timestamps_shift, '<u8')
     ovfl       = _mask_shift(records, 0xC0000000, 30, '<u1')
     # recompute times for overflows w/ invalid photons
     timestamps += np.left_shift(np.cumsum((ovfl == 0b11)*np.bitwise_and(records, 0x0FFFFFFF)), 12)
@@ -532,7 +532,7 @@ def _read_32_nonQC_records(records:np.ndarray[np.uint32],
     ovfl = ovfl[valid]
     del valid # save memory
     # compute single time overflows
-    timestamps += np.left_shift(np.cumsum(ovfl, dtype='u8'), 12)
+    timestamps += np.left_shift(np.cumsum(ovfl, dtype='<u8'), 12)
     del ovfl # save memory
     # get routing (marker/detector) and nanotimes arrays
     # do this only after removing invalid from records to save memory
@@ -646,7 +646,7 @@ def _read_QCX04(file, meta:dict=None)->dict:
     timestamps_unit, nroute = _read_QC_header(file)
     records = np.fromfile(file, dtype='<u4')
     # read final 2 bits, which indicate the record type as photon, marker, macrotime overflow, or GAP
-    phtype = _mask_shift(records, 0xC0000000, 30, np.uint8)
+    phtype = _mask_shift(records, 0xC0000000, 30, '<u1')
     # read timestamps
     timestamps = np.bitwise_and(records, 0x00000FFF).astype('<i8')
     # adjust timestamps for overflows
@@ -658,7 +658,7 @@ def _read_QCX04(file, meta:dict=None)->dict:
     phtype = phtype[ovfl_mask]
     del ovfl_mask # save memory
     # read nanotimes
-    nanotimes = _mask_shift(records, 0x0FFF0000, 16, 'u2')
+    nanotimes = _mask_shift(records, 0x0FFF0000, 16, '<u2')
     # combine CH bits and routing bits for detectors channel
     route_ch = _mask_shift(records, 0x30000000, 28, '<u1')
     route_ch += _mask_shift(records, 0x0000F000, 10, '<u1')

phconvert/hdf5.py
@@ -160,7 +160,9 @@ def _NW_repl(n, descr):
             descr = descr.replace('{NW}', str(n))
     return descr, drop, has_nw
 
+
 _repl_funcs = (_NTH_repl, _DA_repl, _NW_repl)
+
 
 def _format_descr(matchobj, descr):
     descrchunks = descr.split('!!')
@@ -354,6 +356,8 @@ class _SpecDict:
     def _validate_HDF5(self, file, err_pool, warn_pool, descr_pool):
         for node in file.walk_nodes('/'):
             # get field information
+            if 'user' in node._v_pathname.split('/') or node._v_name == 'user':
+                continue
             descr, spec, field_match_obj, err_msg, warn_msg = self._get_subindex(node._v_pathname)
             # check for no non-spec fields
             if warn_msg:
@@ -375,8 +379,6 @@ class _SpecDict:
                 title = node._v_title
                 title = str(title) if isinstance(title, (str, np.str_)) else str(title.decode())
                 if descr != title:
-                    print('descr:', descr)
-                    print('title:', title)
                     descr_msg = 'Description (TITLE) for "%s" not compliant.' % node._v_pathname
                     if isinstance(descr_pool, list):
                         descr_pool.append(descr_msg)
@@ -436,7 +438,6 @@ class _SpecDict:
         finally:
             if close:
                 h5.close()
-
             err_pool = list() if err_pool is None else err_pool
             if strict:
                 if isinstance(warn_pool, list):
@@ -558,16 +559,16 @@ def _h5_write_array(group, name, obj, descr=None, chunked=False, h5file=None):
     if name in h5file.get_node(group):
         # Remove a pre-existing node with the same name
         h5file.remove_node(group, name)
-    save(group, name, obj=obj)
+    save(group, name, obj=obj, title=descr)
     # Set title through property access to work around pytable issue
     # under python 3 (https://github.com/PyTables/PyTables/issues/469)
-    node = h5file.get_node(group)._f_get_child(name)
-    # Ensure descr is a bytes object
-    if descr is None:
-        descr = _EMPTY
-    elif isinstance(descr, (str, np.str_)):
-            descr = descr.encode()
-    node.title = descr
+    # node = h5file.get_node(group)._f_get_child(name)
+    # # Ensure descr is a bytes object
+    # if descr is None:
+    #     descr = _EMPTY
+    # elif isinstance(descr, (str, np.str_)):
+    #         descr = descr.encode()
+    # node.title = descr
 
 
 def _iter_hdf5_dict(data_dict, prefix_list=None, user_descr=None, debug=False):

phconvert/loader.py
@@ -96,11 +96,11 @@ def loadfile_sm(filename, software='LabVIEW Data Acquisition usALEX',
         setup['detectors'] = {'id':unique_detectors, 
                               'label':np.array([''.join(chr(c) for c in chan) 
                                                 for chan in  labels])}
-    provenance = dict(filename=filename, software=software)
+    provenance = dict(filename=str(filename), software=software)
     acquisition_duration = (timestamps[-1] - timestamps[0]) * 12.5e-9
     identity = dict(author=None, author_affiliation=None)
     data = dict(
-        _filename = filename,
+        _filename = str(filename),
         description = None,
         acquisition_duration = round(acquisition_duration),
         photon_data = photon_data,
@@ -270,7 +270,7 @@ def loadfile_ptu(filename:str):
 
     """
     load_pq = {'ptu': pqreader.load_ptu, 'ht3': pqreader.load_ht3,
-               'pt3': pqreader.load_pt3}[filename[-3:]]
+               'pt3': pqreader.load_pt3}[os.path.splitext(filename)[1][1:]]
     
     times, dets, dtime, metadata, marker_ids = load_pq(filename)
     # Creation time from the file header
@@ -280,7 +280,7 @@ def loadfile_ptu(filename:str):
     software_version = metadata.pop('software_version')
     
     provenance = dict(
-        filename=filename,
+        filename=str(filename),
         creation_time=creation_time,
         software=software,
         software_version=software_version,
@@ -315,14 +315,14 @@ def loadfile_ptu(filename:str):
         excitation_cw = None,
         detection_wavelengths = None,
         excitation_alternated = None,
-        detectors = dict(id=det_ids, counts=det_counts, label=None),
+        detectors = {'id':det_ids, 'counts':det_counts, 'label':None},
         )
     identity = dict(author=None, author_affiliation=None)
 
     if dtime is not None:
         laser_repetition_rate = float(metadata.pop('laser_repetition_rate'))
         tcspc_unit = float(metadata.pop('nanotimes_unit'))
-        tcspc_num_bins = 4096
+        tcspc_num_bins = 1<<metadata.pop('nanotimes_bits')
         tcspc_range = tcspc_num_bins * tcspc_unit
         photon_data['nanotimes'] = dtime
         photon_data['measurement_specs']['laser_repetition_rate'] = (laser_repetition_rate,)
@@ -337,7 +337,7 @@ def loadfile_ptu(filename:str):
         photon_data['measurement_specs']['detectors_specs']['non_photon_id1'] = marker_ids
 
     data = dict(
-        _filename = filename,
+        _filename = str(filename),
         acquisition_duration = acquisition_duration,
         photon_data = photon_data,
         setup = setup,
@@ -359,7 +359,7 @@ def usalex_sm(
     This dictionary can be passed to the :func:`phconvert.hdf5.save_photon_hdf5`
     function to save the data in Photon-HDF5 format.
     """
-    print(" - Loading '%s' ... " % filename)
+    print(" - Loading '%s' ... " % (filename))
     timestamps, detectors, labels = smreader.load_sm(filename,
                                                      return_labels=True)
     print(" [DONE]\n")
@@ -391,10 +391,10 @@ def usalex_sm(
         detection_wavelengths = detection_wavelengths,
         excitation_alternated=[True, True])
 
-    provenance = dict(filename=filename, software=software)
+    provenance = dict(filename=str(filename), software=software)
     acquisition_duration = (timestamps[-1] - timestamps[0]) * 12.5e-9
     data = dict(
-        _filename = filename,
+        _filename = str(filename),
         acquisition_duration = round(acquisition_duration),
         photon_data = photon_data,
         setup = setup,

phconvert/pqreader.py
@@ -170,6 +170,7 @@ def load_ptu(filename, return_marker=False, ovcfunc='auto'):
         'tags': _convert_multi_tags(tags)}
     if spec['T'] == 3:
         meta['nanotimes_unit'] = tags['MeasDesc_Resolution']['value']
+        meta['nanotimes_bits'] = spec['dtime_bit']
         meta['laser_repetition_rate'] = tags['TTResult_SyncRate']['value']
     times, dets, dtime, marker_ids = process_pturecords(records, spec, ovcfunc=ovcfunc)
     return times, dets, dtime, meta, marker_ids
@@ -1069,7 +1070,7 @@ def _overflow_correction_HT3_base(times, dets, dtime, channel_bit, wraparound, v
     times = times[~overflows]
     dets = dets[~overflows]
     dtime = dtime[~overflows]
-    times += overflow_correction
+    times += overflow_correction.astype(times.dtype)
     markers = np.bitwise_and(dets, 2**(channel_bit-1)).astype(bool)
     marker_ids = np.unique(dets[markers])
     return times, dets, dtime, marker_ids
@@ -1090,7 +1091,7 @@ def _overflow_correction_HT2_base(times, dets, channel_bit, wraparound, version)
     overflow_correction = overflow_correction[~overflows]
     times = times[~overflows]
     dets = dets[~overflows]
-    times += overflow_correction
+    times += overflow_correction.astype(times.dtype)
     markers = np.bitwise_and(dets, 2**(channel_bit-1)).astype(bool)
     marker_ids = np.unique(dets[markers])
     return times, dets, marker_ids
@@ -1103,7 +1104,7 @@ def _overflow_correction_PT3_base(times, dets, dtime, channel_bit, wraparound, v
     """
     mask = (dets==2**channel_bit-1)
     overflow_mask = (dtime == 0) * mask
-    times += np.cumsum(overflow_mask, dtype=np.uint64)*wraparound
+    times += np.cumsum(overflow_mask, dtype=times.dtype)*wraparound
     times = times[~overflow_mask]
     dets = dets[~overflow_mask]
     dtime = dtime[~overflow_mask]
@@ -1333,15 +1334,16 @@ def process_t3records(t3records, time_bit=10, dtime_bit=15,
         A 3-element tuple containing the following 1D arrays (all of the same
         length):
 
+        - **detectors** (*arrays of uint8*): detector number. When
+          `special_bit = True` the highest bit in `detectors` will be
+          the special bit.
         - **timestamps** (*array of int64*): the macro-time (or number of sync)
           of each photons after overflow correction. Units are specified in
           the file header.
         - **nanotimes** (*array of uint16*): the micro-time (TCSPC time), i.e.
           the time lag between the photon detection and the previous laser
           sync. Units (i.e. the bin width) are specified in the file header.
-        - **detectors** (*arrays of uint8*): detector number. When
-          `special_bit = True` the highest bit in `detectors` will be
-          the special bit.
+        
     """
 
     """

tests/test_pqreader.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 import phconvert as phc
 
-DATADIR = 'notebooks/data/'
+DATADIR = '../PhotonHDF5_testdata/'
 
 
 def dataset1():
@@ -36,8 +36,14 @@ def dataset5():
     return fname
 
 
+def dataset6():
+    fn = 'RawData.ptu' # 'rtGenericT3'
+    fname = DATADIR + fn
+    return fname
+
+
 @pytest.fixture(scope="module",
-                params=[dataset1, dataset2, dataset3, dataset4, dataset5])
+                params=[dataset1, dataset2, dataset3, dataset4, dataset5, dataset6])
 def filename(request):
     fname = request.param()
     return fname
@@ -97,7 +103,8 @@ def test_ptu_overflow_correction(filename):
     timestamps, detectors, nanotimes, meta, marker_ids = phc.pqreader.load_ptu(filename, ovcfunc='base')
     timestamps2, detectors2, nanotimes2, meta2, marker_ids2 = phc.pqreader.load_ptu(
         filename, ovcfunc='numba')
-    assert (timestamps == timestamps2).all()
+    assert np.all(timestamps == timestamps2)
+
 
 def test_load_ptu(filename):
     """Test consistency of data loaded from PTU files."""
@@ -120,7 +127,7 @@ def test_load_ptu(filename):
     if nanotimes is not None:
         assert 'laser_repetition_rate' in meta
         assert 1e6 < meta['laser_repetition_rate'] > 10e6
-        assert ((nanotimes >= 0) * (nanotimes < 4096)).all()
+        assert np.all((nanotimes >= 0) * (nanotimes < 4096))
 
 
 def test_load_ht3():
@@ -132,7 +139,7 @@ def test_load_ht3():
     acq_duration = meta['header']['Tacq'] * 1e-3
     acq_duration2 = (timestamps[-1] - timestamps[0]) * meta['timestamps_unit']
     assert abs(acq_duration - acq_duration2) < 0.1
-    assert (np.diff(timestamps) >= 0).all()
+    assert np.all(np.diff(timestamps) >= 0)
 
 
 def test_load_pt3():
@@ -154,12 +161,12 @@ def test_load_phu():
     filename = DATADIR + fn
     assert os.path.isfile(filename), 'File not found: %s' % filename
     hist, bin_size, meta = phc.pqreader.load_phu(filename)
-    
+
     # test acquisition duration
     acq_duration = meta['tags']['MeasDesc_AcquisitionTime']['value'] * 1e-3
     acq_duration2 = meta['acquisition_duration']
     assert acq_duration == acq_duration2 == 10
-    
+
     # test hist.shape
     tags = meta['tags']
     num_curves = tags['HistoResult_NumberOfCurves']['value']
@@ -176,6 +183,3 @@ def test_load_phu():
     # test `bin_size`
     bin_size2 = [tag['value'] for tag in tags['HistResDscr_MDescResolution']]
     assert np.array_equal(bin_size, bin_size2)
-    
-
-   